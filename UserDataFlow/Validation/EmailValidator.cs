d0435824536021e1b2b56ef4e30fa2426a20525b
Logesh Palani <logesh.01@hotmail.com>
2021-04-08 15:53:36 +0530

    Email validator added

---
+++ UserDataFlow/Validation/EmailValidator.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserDataFlow
+{
+    public static class EmailValidator
+    {
+        public static bool IsValidEmail(string email)
+        {
+            var regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            var match = regex.Match(email);
+            return match.Success;
+        }
+    }
+}
